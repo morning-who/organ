f4e46b498295e5a5b16c21aa6d0f9ccca6e096f8
morning-who <160821389+morning-who@users.noreply.github.com>
2024-10-22 22:39:59 +0530

    script2.js

--- script2.js
+++ script2.js
@@ -1,18 +1,14 @@
-// script.js
-
-// Add event listener to form submission
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
 
-    // Get form data
+
     const formData = new FormData(document.querySelector('form'));
 
-    // Convert form data to JSON
+
     const jsonData = {};
     formData.forEach((value, key) => {
         jsonData[key] = value;
     });
 
-    // Log JSON data to console
     console.log(jsonData);
 });
